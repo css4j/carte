eaffc4615ae8ed1347dc04034506c659e56b3238
Carlos Amengual <carlosame644+github@gmail.com>
2025-10-17 22:29:02 +0200

    Update the benchmark example

--- carte-jmh/examples/benchmark-charts.sh
+++ carte-jmh/examples/benchmark-charts.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
+# Remember to first run ./gradlew uberjar to obtain the carte-jmh-all.jar file
 CARTE_VERSION=0.1.0-SNAPSHOT
 java -jar ../build/libs/carte-jmh-${CARTE_VERSION}-all.jar --config=dom-benchmark.xml "$@"
